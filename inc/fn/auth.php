dab6eefc24cc17dd63246aa7e3606c25a888816f
Sheelpathak <pathaksheel64@gmail.com>
2022-11-18 23:24:40 +0530

    hey

--- inc/fn/auth.php
+++ inc/fn/auth.php
@@ -1,12 +1,15 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include "inc/db/mysql.php";
 session_start();
 
 if (isset($_SESSION["user_id"])) {
     $get_user = mysqli_safe_query("SELECT * FROM users WHERE id = %d", $_SESSION["user_id"]);
     if (!mysqli_num_rows($get_user)) {
-        header("Location: login");
+        header("Location: logout");
     }
 
     $user = mysqli_fetch_object($get_user);
